50edc48c3cf566e4607c8a832d3f3ac9cc963066
Usuário Convidado <Guest@L2304MICRO13.local>
2016-03-15 20:52:59 -0300

    commit inicial

---
+++ ios/MapaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapaViewController.swift
+//  Dog Walker
+//
+//  Created by Usuário Convidado on 15/03/16.
+//  Copyright © 2016 FIAP. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapaViewController: UIViewController, MKMapViewDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
